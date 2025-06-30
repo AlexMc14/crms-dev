fbcba2bc34291f8a294af715a559d5ca9bcaeefe
AlexMc14 <63048415+AlexMc14@users.noreply.github.com>
2025-06-30 17:26:34 +0200

    Add files via upload

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+import { register } from 'register-service-worker';
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      );
+    },
+    registered () {
+      console.log('Service worker has been registered.');
+    },
+    cached () {
+      console.log('Content has been cached for offline use.');
+    },
+    updatefound () {
+      console.log('New content is downloading.');
+    },
+    updated (registration) {
+      console.log('New content is available; refreshing automatically.');
+      // caches.keys().then((cacheNames) => {
+      //  cacheNames.forEach((cacheName) => {
+      //    caches.delete(cacheName);
+      //  });
+      // }).then(() => {
+      //  window.location.reload();
+      // });
+      if (registration.waiting) {
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      }
+      // Refrescar la página para cargar la última versión
+      // window.location.reload();
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.');
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error);
+    }
+  });
+}

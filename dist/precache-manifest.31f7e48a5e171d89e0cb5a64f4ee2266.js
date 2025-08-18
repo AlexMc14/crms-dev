14d79f46438f0f7943f359b78761de3810cc7c55
AlexMarcos <alejandro.marcos@brooklynfitboxing.com>
2025-08-18 22:40:37 +0200

    Build de producción 2025-08-18 22:40:37

---
+++ dist/precache-manifest.31f7e48a5e171d89e0cb5a64f4ee2266.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459e1d980ebd07f4fa39ea173f223b2a",
+    "url": "/crms-front/_redirects"
+  },
+  {
+    "revision": "ef04733414a6329f7dfb",
+    "url": "/crms-front/assets/css/app.7d581b51.css"
+  },
+  {
+    "revision": "9b18eedb31c8fee194d0",
+    "url": "/crms-front/assets/css/chunk-2d067b96.f3b34b67.css"
+  },
+  {
+    "revision": "51d794a6bc2f4e21af2c",
+    "url": "/crms-front/assets/css/chunk-cd879d44.b6ef748b.css"
+  },
+  {
+    "revision": "9eab445108b67d5d8769",
+    "url": "/crms-front/assets/css/chunk-vendors.6f4eca19.css"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/crms-front/assets/fonts/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/crms-front/assets/fonts/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/crms-front/assets/fonts/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "559c24220461c46b32cfbded5e9b41f7",
+    "url": "/crms-front/assets/img/png.559c2422.png"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/crms-front/assets/img/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "5656a2c24ff1fd56a47d2df656f18126",
+    "url": "/crms-front/assets/img/word.5656a2c2.png"
+  },
+  {
+    "revision": "ef04733414a6329f7dfb",
+    "url": "/crms-front/assets/js/app.061e164a.js"
+  },
+  {
+    "revision": "9b18eedb31c8fee194d0",
+    "url": "/crms-front/assets/js/chunk-2d067b96.3ac464b0.js"
+  },
+  {
+    "revision": "8954f1ded5b28b4d2241",
+    "url": "/crms-front/assets/js/chunk-2d219ff9.aa1513ee.js"
+  },
+  {
+    "revision": "51d794a6bc2f4e21af2c",
+    "url": "/crms-front/assets/js/chunk-cd879d44.e1089ecf.js"
+  },
+  {
+    "revision": "9eab445108b67d5d8769",
+    "url": "/crms-front/assets/js/chunk-vendors.f5a82942.js"
+  },
+  {
+    "revision": "9fdf1ba054e75dca30d0deaa805ba757",
+    "url": "/crms-front/documents/baremoTrafico2016.pdf"
+  },
+  {
+    "revision": "f63454c11a19ed14920fd7b70c04fed3",
+    "url": "/crms-front/documents/plantillaInformePericial.docx"
+  },
+  {
+    "revision": "ae6aa312a7e26ba29fdeb0eecf0b4610",
+    "url": "/crms-front/documents/plantillaVDC.docx"
+  },
+  {
+    "revision": "8150b820b20fb1787b0f2f8a6b5e0886",
+    "url": "/crms-front/documents/plantillaViabilidad.docx"
+  },
+  {
+    "revision": "7c8f59fa0200d588ca162a015beeee3a",
+    "url": "/crms-front/documents/plantillaViabilidadIncapacidad.docx"
+  },
+  {
+    "revision": "3dd1834e303f20d1f926ad74319a9920",
+    "url": "/crms-front/documents/plantillainformepericialincapacidad.doc"
+  },
+  {
+    "revision": "5bc219a793634a5863a2a8c8a5f1bfb0",
+    "url": "/crms-front/favicon.png"
+  },
+  {
+    "revision": "50052cf367e47463f343e43f37a417d6",
+    "url": "/crms-front/favicon.svg"
+  },
+  {
+    "revision": "95e9e473b17de8a114939fe70ac19c75",
+    "url": "/crms-front/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "9078db0ba9873192fece04968eb64f2a",
+    "url": "/crms-front/index.html"
+  },
+  {
+    "revision": "9b04795ddbc017f8dca2a2f63c86257c",
+    "url": "/crms-front/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crms-front/robots.txt"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crms-front/precache-manifest.31f7e48a5e171d89e0cb5a64f4ee2266.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "Informes-Medicos-Periciales"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});

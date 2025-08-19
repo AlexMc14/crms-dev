cd1ce4b7deb9dced4f4bf74ac2fc3f3c0bc71327
AlexMc14 <63048415+AlexMc14@users.noreply.github.com>
2025-08-19 20:55:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crms-dev/precache-manifest.05981e792090cf6d679e5d6a74f23685.js"
+  "/crms-dev/precache-manifest.75a4d6456837db096795d42034c610e0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Informes-Medicos-Periciales"});

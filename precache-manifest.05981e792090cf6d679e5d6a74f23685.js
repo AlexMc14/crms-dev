c291fb608ad376ae7755d38e41000e186b4a5da9
AlexMc14 <63048415+AlexMc14@users.noreply.github.com>
2025-08-19 10:09:06 +0200

    Add files via upload

---
+++ precache-manifest.05981e792090cf6d679e5d6a74f23685.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459e1d980ebd07f4fa39ea173f223b2a",
+    "url": "/crms-dev/_redirects"
+  },
+  {
+    "revision": "1d4ac4117de22246505c",
+    "url": "/crms-dev/assets/css/app.7d581b51.css"
+  },
+  {
+    "revision": "9b18eedb31c8fee194d0",
+    "url": "/crms-dev/assets/css/chunk-2d067b96.f3b34b67.css"
+  },
+  {
+    "revision": "51d794a6bc2f4e21af2c",
+    "url": "/crms-dev/assets/css/chunk-cd879d44.b6ef748b.css"
+  },
+  {
+    "revision": "1169fe6c442d824ef2a2",
+    "url": "/crms-dev/assets/css/chunk-vendors.6f4eca19.css"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/crms-dev/assets/fonts/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/crms-dev/assets/fonts/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/crms-dev/assets/fonts/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "559c24220461c46b32cfbded5e9b41f7",
+    "url": "/crms-dev/assets/img/png.559c2422.png"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/crms-dev/assets/img/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "5656a2c24ff1fd56a47d2df656f18126",
+    "url": "/crms-dev/assets/img/word.5656a2c2.png"
+  },
+  {
+    "revision": "1d4ac4117de22246505c",
+    "url": "/crms-dev/assets/js/app.fcfaa182.js"
+  },
+  {
+    "revision": "9b18eedb31c8fee194d0",
+    "url": "/crms-dev/assets/js/chunk-2d067b96.3ac464b0.js"
+  },
+  {
+    "revision": "8954f1ded5b28b4d2241",
+    "url": "/crms-dev/assets/js/chunk-2d219ff9.aa1513ee.js"
+  },
+  {
+    "revision": "51d794a6bc2f4e21af2c",
+    "url": "/crms-dev/assets/js/chunk-cd879d44.e1089ecf.js"
+  },
+  {
+    "revision": "1169fe6c442d824ef2a2",
+    "url": "/crms-dev/assets/js/chunk-vendors.47098502.js"
+  },
+  {
+    "revision": "9fdf1ba054e75dca30d0deaa805ba757",
+    "url": "/crms-dev/documents/baremoTrafico2016.pdf"
+  },
+  {
+    "revision": "f63454c11a19ed14920fd7b70c04fed3",
+    "url": "/crms-dev/documents/plantillaInformePericial.docx"
+  },
+  {
+    "revision": "ae6aa312a7e26ba29fdeb0eecf0b4610",
+    "url": "/crms-dev/documents/plantillaVDC.docx"
+  },
+  {
+    "revision": "8150b820b20fb1787b0f2f8a6b5e0886",
+    "url": "/crms-dev/documents/plantillaViabilidad.docx"
+  },
+  {
+    "revision": "7c8f59fa0200d588ca162a015beeee3a",
+    "url": "/crms-dev/documents/plantillaViabilidadIncapacidad.docx"
+  },
+  {
+    "revision": "3dd1834e303f20d1f926ad74319a9920",
+    "url": "/crms-dev/documents/plantillainformepericialincapacidad.doc"
+  },
+  {
+    "revision": "5bc219a793634a5863a2a8c8a5f1bfb0",
+    "url": "/crms-dev/favicon.png"
+  },
+  {
+    "revision": "50052cf367e47463f343e43f37a417d6",
+    "url": "/crms-dev/favicon.svg"
+  },
+  {
+    "revision": "95e9e473b17de8a114939fe70ac19c75",
+    "url": "/crms-dev/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "f5ba9dee57c9dd7b144c540d964ce72e",
+    "url": "/crms-dev/index.html"
+  },
+  {
+    "revision": "9b04795ddbc017f8dca2a2f63c86257c",
+    "url": "/crms-dev/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crms-dev/robots.txt"
+  }
+]);
